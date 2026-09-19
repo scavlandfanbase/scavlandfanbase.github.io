45a88958103cfbaa4c842898207cd223527b4699
scavlandfanbase <scavlandfanbase@gmail.com>
2026-09-19 09:00:03 +0100

    Move Items navigation after Map

--- site-shell.js
+++ site-shell.js
@@ -3,7 +3,7 @@ function buildShell(){
  document.querySelectorAll('.scav-header,.site-nav,.nav-bar').forEach(el=>el.remove());
  [...document.body.children].filter(el=>el.tagName==='HEADER'&&el.querySelector('nav')).forEach(el=>el.remove());
  const page=(location.pathname.split('/').pop()||'index.html').toLowerCase();
- const links=[['index.html','Home'],['items.html','Items'],['vendors.html','Vendors'],['weapons.html','Weapons'],['armour.html','Armour'],['crafting.html','Crafting'],['map.html','Map'],['factions.html','Factions'],['areas.html','Areas']];
+ const links=[['index.html','Home'],['vendors.html','Vendors'],['weapons.html','Weapons'],['armour.html','Armour'],['crafting.html','Crafting'],['map.html','Map'],['items.html','Items'],['factions.html','Factions'],['areas.html','Areas']];
  const header=document.createElement('header');header.className='scav-site-header';
  header.innerHTML='<img class="scav-site-logo" src="images/branding/Scavland_Logo_2025.png" alt="SCAVLAND"><div class="scav-site-subtitle">Wasteland Survival Database</div>';
  const nav=document.createElement('nav');nav.className='scav-site-nav';nav.setAttribute('aria-label','Main navigation');
